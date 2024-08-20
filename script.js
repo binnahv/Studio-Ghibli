d334e2b2eac7d111d2dd59a455485f362bc238c1
Sabrina Vidal <binnahv@gmail.com>
2024-08-20 17:13:29 -0300

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,22 +1,12 @@
-// Substitua 'YOUR_API_KEY' pela sua chave da TMDb API
-const apiKey = 'YOUR_API_KEY';
+const filmesList = document.getElementById('filmes');
 
-fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`)
-  .then(response => response.json())
-  .then(data => {
-    const movies = data.results;
-    const movieContainer = document.querySelector('.movie-container');
-
-    movies.forEach(movie => {
-      const movieElement = document.createElement('div');
-      movieElement.classList.add('movie');
-
-      movieElement.innerHTML = `
-        <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="${movie.title}">
-        <h2>${movie.title}</h2>
-        <p>${movie.overview}</p>
-      `;
-
-      movieContainer.appendChild(movieElement);
+fetch('https://ghibliapi.vercel.app/films')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(filme => {
+            const li = document.createElement('li');
+            li.classList.add('filme');
+            li.textContent = filme.title;
+            filmesList.appendChild(li);
+        });
     });
-  });
