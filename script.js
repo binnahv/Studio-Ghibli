3c6f6414bc261f54df316690ff244a2736fbbf1b
Sabrina Vidal <binnahv@gmail.com>
2024-09-05 09:02:30 -0300

    modified:   script.js

--- script.js
+++ script.js
@@ -79,3 +79,16 @@ function performSearch() {
         displayFilms(window.allFilms);
     }
 }
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+      caches.match(event.request).then(response => {
+        return response || fetch(event.request).then(fetchResponse => {
+          return caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, fetchResponse.clone());
+            return fetchResponse;
+          });
+        });
+      })
+    );
+  });
